4309a955a5e8ac884de345c2756c4e14dea631e9
patrickcap <62596801+patrickcap@users.noreply.github.com>
2023-12-20 13:47:51 +0000

    got large data, rate limited for small data

--- research/open_aq_search.py
+++ research/open_aq_search.py
@@ -11,10 +11,8 @@ import requests
 OPEN_AQ_API_KEY = "d568965fba34916d82780a672e32d0029f0e45f5293dc4f45a1ae33fe827623b"
 
 # -------------------- CONFIG --------------------
-lat = "38.9074"         # Latitude [deg]
-long = "-77.0373"       # Longitude [deg]
-radius = "1600"          # Search radius [m]
-loc_result_limit = "100"   # Max number of location results returned
+radius = "10000"          # Search radius [m]
+loc_result_limit = "1"   # Max number of location results returned
 measurement_result_limit = "10"  # max number of measurement results returned for a location (for some reason <=2 causes a 402 timeout)
 # ------------------------------------------------
 
@@ -27,7 +25,7 @@ def long_lat_conv(degrees: int, minutes: int, seconds: int, direction: str) -> f
 
     """
     dd = float(degrees) + float(minutes)/60 + float(seconds)/(60*60)
-    if direction == 'E' or direction == 'S':
+    if direction == 'W' or direction == 'S':
         dd *= -1
     return dd
 
@@ -52,58 +50,50 @@ for index in range(len(data)):
     if curr_name != "N/A":
         curr_lat = long_lat_conv(int(curr_airport[5]), int(curr_airport[6]), int(curr_airport[7]), curr_airport[8])
         curr_long = long_lat_conv(int(curr_airport[9]), int(curr_airport[10]), int(curr_airport[11]), curr_airport[12])
-        airports_list.append([curr_name, curr_lat, curr_long])
+        print(str(round((index / len(data)) * 100, 1)) + "%", curr_name, curr_lat, curr_long, flush=True)
+
+        loc_url = "https://api.openaq.org/v3/locations?order_by=id&sort_order=asc&coordinates=" + str(curr_lat) + "%2C" + str(curr_long) + "&radius=" + radius + "&limit=" + loc_result_limit + "&page=1"
+        loc_res_json = requests.get(loc_url, headers={"X-API-Key": OPEN_AQ_API_KEY})
+        loc_res_obj = loc_res_json.json()
+
+        # If at least one result was found
+        if "results" in loc_res_obj:
+            loc_results = loc_res_obj["results"]
+            # print("x->", loc_results)
+            pm10_list = []
+            cumulative_sum_pm10 = 0
+            num_measurements_pm10 = 0
+            for loc_result in loc_results:
+                curr_id = loc_result["id"]
+                curr_name = loc_result["name"]
+                curr_measurement_url = "https://api.openaq.org/v3/locations/" + str(curr_id) + "/measurements?period_name=hour&limit=" + measurement_result_limit + "&page=1"
+                measurement_res_json = requests.get(curr_measurement_url, headers={"X-API-Key": OPEN_AQ_API_KEY})
+                measurement_res_obj = measurement_res_json.json()
+                if "results" in measurement_res_obj:
+                    measurement_results = measurement_res_obj["results"]
+                    # Search for PM10
+                    for measurement_result in measurement_results:
+                        if measurement_result["parameter"]["name"] == "pm10":
+                            print("---", measurement_result["value"])
+                            # If has PM10, record the distance and value
+                            cumulative_sum_pm10 += measurement_result["value"]
+                            num_measurements_pm10 += 1
+                            pm10_list.append([curr_name, measurement_result["value"]])
+
+            if num_measurements_pm10 > 0:
+                pm10_avg = cumulative_sum_pm10 / num_measurements_pm10            
+                airports_list.append([curr_name, curr_lat, curr_long, pm10_avg])
+
+        elif "details" in loc_res_obj:
+            print("-->", loc_res_obj["details"], flush=True)
+        
+        else:
+            print("--> Other", flush=True)
+            
 
 print(airports_list[0])
 
-airports_df = pd.DataFrame(airports_list, columns=["Airport Name", "Latitude", "Longitude"])
+airports_df = pd.DataFrame(airports_list, columns=["Airport Name", "Latitude", "Longitude", "PM10"])
 print(airports_df)
 
-# Iterate through each airport's lat/long location and ask the OpenAQ API what
-# measurements are within some distance threshold
-# if any are found, choose the closest measurement THAT ALSO has a PM10 parameter
-# to associate to that airport
-
-# aq_measurements = []
-# for index, row in airports_df.iterrows():
-#     curr_url = "https://api.openaq.org/v3/locations?order_by=id&sort_order=asc&coordinates=" + str(row['Latitude']) + "%2C" + str(row['Longitude']) + "&radius=" + radius + "&limit=" + result_limit + "&page=1"
-#     res_json = requests.get(curr_url, headers={"X-API-Key": OPEN_AQ_API_KEY})
-#     res_obj = res_json.json()
-
-
-loc_url = "https://api.openaq.org/v3/locations?order_by=id&sort_order=asc&coordinates=" + lat + "%2C" + long + "&radius=" + radius + "&limit=" + loc_result_limit + "&page=1"
-print(loc_url)
-loc_res_json = requests.get(loc_url, headers={"X-API-Key": OPEN_AQ_API_KEY})
-loc_res_obj = loc_res_json.json()
-
-# If at least one result was found
-loc_results = loc_res_obj["results"]
-# print("x->", loc_results)
-pm10_list = []
-cumulative_sum_pm10 = 0
-num_measurements_pm10 = 0
-for loc_result in loc_results:
-    curr_id = loc_result["id"]
-    curr_name = loc_result["name"]
-    print(curr_name, curr_id)
-
-    curr_measurement_url = "https://api.openaq.org/v3/locations/" + str(curr_id) + "/measurements?period_name=hour&limit=" + measurement_result_limit + "&page=1"
-    measurement_res_json = requests.get(curr_measurement_url, headers={"X-API-Key": OPEN_AQ_API_KEY})
-    measurement_res_obj = measurement_res_json.json()
-    if "detail" not in measurement_res_obj:
-        measurement_results = measurement_res_obj["results"]
-        # Search for PM10
-        for measurement_result in measurement_results:
-            if measurement_result["parameter"]["name"] == "pm10":
-                # If has PM10, record the distance and value
-                cumulative_sum_pm10 += measurement_result["value"]
-                num_measurements_pm10 += 1
-                pm10_list.append([curr_name, measurement_result["value"]])
-
-        print("z->", pm10_list)
-
-if num_measurements_pm10 > 0:
-    pm10_avg = cumulative_sum_pm10 / num_measurements_pm10
-    print(pm10_avg)
-
-
+airports_df.to_csv("airports_pm10.csv", index=True)
