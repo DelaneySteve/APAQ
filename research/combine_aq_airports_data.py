a5324b3b22ec6867e0153ec3d4f88f5e61a37c1c
patrickcap <62596801+patrickcap@users.noreply.github.com>
2024-01-09 10:44:07 +0000

    floats not strs

--- research/combine_aq_airports_data.py
+++ research/combine_aq_airports_data.py
@@ -6,12 +6,12 @@ import json
 import csv
 
 # assign airports json to local object
-with open('airports.json', 'r') as f:
+with open('../data/airports.json', 'r') as f:
     airports_orig = json.load(f)
 
 # assign air quality data to object
 air_qualities = []
-with open('airports_aq_large.csv', 'r') as f:
+with open('../data/airports_aq_large.csv', 'r') as f:
     reader = csv.reader(f)
     for row in reader:
         air_qualities.append(row[0].split("\t"))
@@ -21,14 +21,13 @@ airports_augmented = {"airports": []}
 
 # find matching airports and combine data
 for airport_a in airports_orig["airports"]:
-    print(airport_a)
     iata_a = airport_a["iata"]
     for airport_b in air_qualities:
         iata_b = airport_b[1]
         if iata_a == iata_b:
-            airport_a["air quality"] = airport_b[5]
+            airport_a["air_quality"] = float(airport_b[5])
             airports_augmented["airports"].append(airport_a)
 
 # write Python dictionary to json file
-with open('airports_augmented.json', 'w', encoding='utf-8') as f:
+with open('../data/airports_augmented.json', 'w', encoding='utf-8') as f:
     json.dump(airports_augmented, f, ensure_ascii=False, indent=4)
