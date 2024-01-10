de787c0c6f7c9013130d3b6a9d66c7713f3f4857
patrickcap <62596801+patrickcap@users.noreply.github.com>
2024-01-10 11:59:18 +0000

    streamlined aq transfer

--- research/combine_aq_airports_data.py
+++ research/combine_aq_airports_data.py
@@ -7,7 +7,7 @@ import csv
 
 # assign airports json to local object
 with open("../data/airports.json", "r") as f:
-    airports_orig = json.load(f)
+    airports_obj = json.load(f)
 
 # assign air quality data to object
 air_qualities = []
@@ -16,11 +16,8 @@ with open("../data/airports_aq_large.csv", "r") as f:
     for row in reader:
         air_qualities.append(row[0].split("\t"))
 
-# new object to store augmented airports list in
-airports_augmented = {"airports": []}
-
 # find matching airports and combine data
-for airport_a in airports_orig["airports"]:
+for airport_a in airports_obj["airports"]:
     # extract airport A
     iata_a = airport_a["iata"]
     for airport_b in air_qualities:
@@ -32,5 +29,5 @@ for airport_a in airports_orig["airports"]:
             airport_a["air_quality"] = float(airport_b[5])
 
 # write Python dictionary to json file
-with open("../data/airports_augmented.json", "w", encoding="utf-8") as f:
-    json.dump(airports_orig, f, ensure_ascii=False, indent=4)
+with open("../data/airports_air_quality.json", "w", encoding="utf-8") as f:
+    json.dump(airports_obj, f, ensure_ascii=False, indent=4)
