bddfafcf362b7576b985b56491d2db76d67449c2
Delaney <delaneyrosario98@gmail.com>
2024-01-11 14:19:46 +0000

    save dataframe

--- data/main.py
+++ data/main.py
@@ -1,26 +1,27 @@
 import json
 import pandas as pd
-from  data.stats import runway_stats
-from data.stats import flight_stats
+from  data.stats import get_runway_stats
+from data.stats import get_flight_stats
 from data import json_converter
 
-FILENAME = "C:/Users/delan/Downloads/Airports.json"
+
+
+
+FILENAME = "C:/Users/delan/Documents/APAQ_files/Airports.json"
 with open(FILENAME, "r", encoding="utf-8") as f:
     raw_data = json.load(f)
-airportData = json_converter.DataConverter(raw_data)
 
+airportData = json_converter.DataConverter(raw_data)
 airport_df = airportData.airports_df
 flights_json = airportData.flights
 runways_json = airportData.runways
 
-runways_stats = runway_stats.RunwayStats(runways_json)
-flights_stats = flight_stats.FlightStats(flights_json)
+runways_stats = get_runway_stats.RunwayStats(runways_json)
+flights_stats = get_flight_stats.FlightStats(flights_json)
 runway_df = runways_stats.runway_df()
 flights_df = flights_stats.flight_count_df()
 
 full_airports_df = pd.concat([airport_df,runway_df,flights_df], axis= 1)
 
-# dropping null data for now
-full_airports_df = full_airports_df.dropna()
-full_airports_df["altitude"] = full_airports_df["altitude"].astype(float)
-print(full_airports_df)
+SAVE_FILE = "C:/Users/delan/Documents/APAQ_files/final_airport_df.csv"
+full_airports_df.to_csv(SAVE_FILE, sep=",", index=False, encoding="utf-8")
