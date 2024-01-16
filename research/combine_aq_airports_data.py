b6bc7c955c39475cef6b99fab79d47897d36e53a
patrickcap <62596801+patrickcap@users.noreply.github.com>
2024-01-16 10:14:45 +0000

    config yaml file and updated api definition

--- research/combine_aq_airports_data.py
+++ research/combine_aq_airports_data.py
@@ -1,17 +1,53 @@
 """
 Program to inject air quality data into the airports list from FlightRadar24
+
+IMPORTANT:
+- This file must be run AFTER the "open_weather_search.py" file
+- This file requires a configuration file to run successfully
+- The configuration file must be in the "research" directory and be named "config.yaml"
+- The configuration file must at least contain the following:
+
+file_paths:
+  airports_load: str (path the JSON file containing information about airports, see below)
+  air_quality_load: str (path to air quality measurements obtained from "open_weather_search.py" file)
+  airports_augmented_dump: str (path to airport data with air quality measurements now integrated)
+
+airports_load file structure:
+{
+    airports: [
+        {
+            “altitude”: int
+            “country”: str
+            “iata”: str
+            “icao”: str
+            “name”: str
+        },
+        {},
+        ...
+    ]
+}
 """
 
 import json
 import csv
+import yaml
+from yaml.loader import SafeLoader
+
+# obtain API key from config file
+with open("config.yaml", "r", encoding="utf-8") as f:
+    config = yaml.load(f, Loader=SafeLoader)
+
+AIRPORTS_LOAD_PATH = config["file_paths"]["airports"]
+AIR_QUALITY_LOAD_PATH = config["file_paths"]["air_quality"]
+AIRPORTS_DUMP_PATH = config["file_paths"]["airports_augmented"]
 
 # assign airports json to local object
-with open("../data/airports.json", "r") as f:
+with open(AIRPORTS_LOAD_PATH, "r") as f:
     airports_obj = json.load(f)
 
 # assign air quality data to object
 air_qualities = []
-with open("../data/airports_aq_large.csv", "r") as f:
+with open(AIR_QUALITY_LOAD_PATH, "r") as f:
     reader = csv.reader(f)
     for row in reader:
         air_qualities.append(row[0].split("\t"))
@@ -29,5 +65,5 @@ for airport_a in airports_obj["airports"]:
             airport_a["air_quality"] = float(airport_b[5])
 
 # write Python dictionary to json file
-with open("../data/airports_air_quality.json", "w", encoding="utf-8") as f:
+with open(AIRPORTS_DUMP_PATH, "w", encoding="utf-8") as f:
     json.dump(airports_obj, f, ensure_ascii=False, indent=4)
--- research/open_weather_search.py
+++ research/open_weather_search.py
@@ -2,14 +2,32 @@
 Testing the IQAir API to use a list of airport locations
 to search and find air quality measurements within a particular
 distance from those locations.
+
+IMPORTANT:
+- This file must be run BEFORE the "combine_aq_airports_data.py" file
+- This file requires a configuration file to run successfully
+- The configuration file must be in the "research" directory and be named "config.yaml"
+- The configuration file must at least contain the following:
+
+api_keys:
+    open_weather: str (API key to access OpenWeather)
+
 """
 
 import csv
+import logging
 import pandas as pd
 import requests
-from config import OPEN_WEATHER_API_KEY
+import yaml
+from yaml.loader import SafeLoader
 
-FILE_PATH = "../data/iata-icao.csv"
+# obtain API key from config file
+with open("config.yaml", "r", encoding="utf-8") as f:
+    config = yaml.load(f, Loader=SafeLoader)
+
+OPEN_WEATHER_API_KEY = config["api_keys"]["open_weather"]
+AIR_QUALITY_DUMP_PATH = config["file_paths"]["air_quality"]
+AIRPORTS_FILE_PATH = "../data/iata-icao.csv"
 ICAO_INDEX = 3
 IATA_INDEX = 2
 LAT_INDEX = 5
@@ -21,14 +39,15 @@ airports_lat = []
 airports_lon = []
 airports_pm10 = []
 
+
 # Open the file with specified encoding
-with open(FILE_PATH, 'r', encoding='utf-8') as file:
+with open(AIRPORTS_FILE_PATH, "r", encoding="utf-8") as file:
     next(file) # skips header
     csv_reader = csv.reader(file)
     data = []
     for line in csv_reader:
         # Process each line here
-        processed_line = [cell.encode('unicode_escape').decode('utf-8') for cell in line]
+        processed_line = [cell.encode("unicode_escape").decode("utf-8") for cell in line]
         curr_icao = processed_line[ICAO_INDEX]
         curr_iata = processed_line[IATA_INDEX]
         curr_lat = processed_line[LAT_INDEX]
@@ -39,7 +58,7 @@ with open(FILE_PATH, 'r', encoding='utf-8') as file:
         response = requests.get(url)
         if response.status_code == 200:
             result = response.json()
-            # Process 'result' as needed
+            # Process "result" as needed
             try:
                 airports_pm10.append(float(result["list"][0]["components"]["pm10"]))
                 airports_icao.append(curr_icao)
@@ -50,7 +69,7 @@ with open(FILE_PATH, 'r', encoding='utf-8') as file:
             except KeyError:
                 curr_pm10 = None
         else:
-            print("Request failed with status code:", response.status_code)
+            logging.info("Request failed with status code: %s", str(response.status_code))
 
 airports_df = pd.DataFrame({
                             "iata": airports_iata,
@@ -59,6 +78,5 @@ airports_df = pd.DataFrame({
                             "longitude": airports_lon,
                             "pm10": airports_pm10
                             })
-print(airports_df)
 
-airports_df.to_csv("airports_aq_large.csv", sep='\t')
+airports_df.to_csv(AIR_QUALITY_DUMP_PATH, sep="\t")
