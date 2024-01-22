759cdd9864c1fe41df9b49275e9cf016bab6d19d
patrickcap <62596801+patrickcap@users.noreply.github.com>
2024-01-22 16:19:02 +0000

    jsonified response from response object

--- src/api/operations/create_prediction.py
+++ src/api/operations/create_prediction.py
@@ -3,6 +3,8 @@ Provides the API endpoint that makes a prediction on a target variable within a 
 """
 
 from fastapi import APIRouter, HTTPException
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
 from api.resources.airport import Airport
 from api.resources.post_air_quality_response import PostAirQualityResponse
 
@@ -11,10 +13,20 @@ prediction_router = APIRouter(prefix="/air-quality")
 @prediction_router.post("", response_model=PostAirQualityResponse, status_code=201)
 async def predict_air_quality(airport: Airport):
 
-# Temporary check that airport data has been received
-    print("Received airport information:", airport.dict())
+    # Use airport input parameters to create air quality prediction and store as PostAirQualityResponse object
+    air_quality_response = PostAirQualityResponse(air_quality=get_air_quality_prediction(airport))
 
-    # Replace this with your actual air quality prediction result
-    predicted_air_quality = 2.12
+    # Convert the object to a dictionary to allow it to be JSONified
+    response_dict = jsonable_encoder(air_quality_response)
 
-    return {"air_quality": predicted_air_quality}
+    # Return a JSONResponse with the serialized dictionary
+    return JSONResponse(content=response_dict, status_code=201)
+
+def get_air_quality_prediction(airport: Airport):
+    """
+    Accesses air quality prediction model.
+
+    Returns float
+    """
+
+    return 2.12
