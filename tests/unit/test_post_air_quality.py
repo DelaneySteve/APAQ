1b557a24f12bc4a93310e75b1c60be783728cc44
Delaney <delaneyrosario98@gmail.com>
2024-03-26 14:24:49 +0000

    test

--- tests/unit/test_post_air_quality.py
+++ tests/unit/test_post_air_quality.py
@@ -44,8 +44,8 @@ class TestPostAirQuality(unittest.TestCase):
         resp_json = resp.json()
         # There should only be one key in the response
         self.assertCountEqual(['air_quality'], resp_json.keys())
-        self.assertTrue(30 <= resp_json['air_quality'] <= 50)
         print(resp_json['air_quality'])
+        self.assertTrue(30 <= resp_json['air_quality'] <= 50)
 
     def test_predict_with_multiple_runways(self) -> None:
         airport_data = {
@@ -69,8 +69,8 @@ class TestPostAirQuality(unittest.TestCase):
         # There should only be one key in the response
         self.assertCountEqual(['air_quality'], resp_json)
         # Check the air quality is approximately correct
-        self.assertTrue(30 <= resp_json['air_quality'] <= 50)
         print(resp_json['air_quality'])
+        self.assertTrue(30 <= resp_json['air_quality'] <= 50)
 
     def test_can_predict_with_no_runways(self) -> None:
         airport_data = {
@@ -82,8 +82,8 @@ class TestPostAirQuality(unittest.TestCase):
         resp = self.test_client.post('/air-quality', json=airport_data, headers={'X-API-KEY': self.api_key})
         self.assertEqual(resp.status_code, 201)
         self.assertCountEqual(['air_quality'], resp.json())
-        self.assertTrue(30 <= resp.json()['air_quality'] <= 50)
         print(resp.json()['air_quality'])
+        self.assertTrue(30 <= resp.json()['air_quality'] <= 50)
 
     def test_predict_fails_with_bad_api_key(self) -> None:
         api_key = 'some_bad_api_key'
