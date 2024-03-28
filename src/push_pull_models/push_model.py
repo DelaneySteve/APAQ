331037615b31ab8a1ab5f33c47934f4e18b48931
Delaney <delaneyrosario98@gmail.com>
2024-03-28 17:51:06 +0000

    webbrowser

--- src/push_pull_models/push_model.py
+++ src/push_pull_models/push_model.py
@@ -29,7 +29,7 @@ def upload_multipart_model(filename: str, filepath: str) -> str:
             flow = InstalledAppFlow.from_client_secrets_file(
                 'credentials.json', SCOPES
             )
-            creds = flow.run_local_server(port=0)
+            creds = flow.run_console()
             # Save the credentials for the next run
             with open('token.json', 'w') as token: # pylint: disable=unspecified-encoding
                 token.write(creds.to_json())
