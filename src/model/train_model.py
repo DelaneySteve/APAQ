19d83898364f73f6efc38b552543cb422a0b981c
Delaney <delaneyrosario98@gmail.com>
2024-03-28 17:14:37 +0000

    push model google drive

--- src/model/train_model.py
+++ src/model/train_model.py
@@ -2,16 +2,20 @@ import sys
 from argparse import ArgumentParser
 
 from src.model.model import Model
+from src.push_pull_models.push_model import upload_multipart_model
 
 SAVE_MODEL_PATH = './model/rf_model.pickle'
-
+DEFAULT_DATA_PATH = './data/airports_augmented_dataset.json'
 
 def setup_parser() -> ArgumentParser:
     parser = ArgumentParser(description='Read file from Command line.')
     parser.add_argument(
         '--airports_augmented_dataset',
-        required=True,
+        nargs = '?',
+        const = DEFAULT_DATA_PATH,
+        required=False,
         type=str,
+        default = DEFAULT_DATA_PATH,
         help='file path for information about each airport',
     )
     return parser
@@ -23,7 +27,7 @@ def main(args: list[str]) -> None:
     rf_model = Model()
     rf_model.train(args.airports_augmented_dataset)
     rf_model.save_trained_model(SAVE_MODEL_PATH)
-
-
+    model_id = upload_multipart_model('rf_model.pickle', SAVE_MODEL_PATH)
+    print(model_id)
 if __name__ == '__main__':
     main(sys.argv[1:])
---
+++ src/push_pull_models/push_model.py
@@ -0,0 +1,57 @@
+import logging
+import os
+import os.path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from googleapiclient.http import MediaFileUpload
+
+logging.basicConfig(level=logging.INFO)
+
+SCOPES = ['https://www.googleapis.com/auth/drive.file']
+MODEL_FOLDER_ID = '1zsMUAaFUNvZpNGA-6yvUTHKyZCS49Za6'
+
+
+def upload_multipart_model(filename: str, filepath: str) -> str:
+    creds = None
+
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES
+            )
+            creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            with open('token.json', 'w') as token: # pylint: disable=unspecified-encoding
+                token.write(creds.to_json())
+    try:
+        # create drive api client
+        service = build('drive', 'v3', credentials=creds)
+        file_metadata = {'name': filename,  'parents': [MODEL_FOLDER_ID]}
+        media = MediaFileUpload(filepath, mimetype='model/pickle')
+        file = service.files().create(body=file_metadata, media_body=media).execute()
+        logging.info(' File uploaded to Google Drive uploaded_models folder under ID: %s.', file.get('id'))
+
+        media = None
+
+    except HttpError as error:
+        print(f' An error occurred trying to upload: {error}')
+        file = None
+
+    # now remove the file from the local repo
+    try:
+        os.remove(filepath)
+        logging.info(' File: "%s" deleted.', filepath)
+    except PermissionError as err:
+        logging.error(' Failed to delete local file. Looks like a permission error: %s', err)
+    except OSError as err:
+        logging.warning(' Failed to delete local file: OS error occurred: %s', err)
+
+    return file.get('id')
